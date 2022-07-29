61cb0e34b2ea7b6b64b44e22be58a19d9c6cc952
biffpixel <biffpixel@biglifepixels.com>
2022-07-28 15:29:50 -1000

    Added a few lines to the starting log

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("---------------------------------------");
 console.log("Raytracing in a Weekend - Peter Shirley");
 console.log("---------------------------------------");
-console.log("NodeJS implementation by Merbin J Anselm");
+console.log("NodeJS implementation - Merbin J Anselm");
+console.log("---------------------------------------");
+console.log("Web containerization - Barry Ruff");
+console.log
 console.log("\nStart running exercises by executing following command(s)");
 console.log("> npm run ex<EXERCISE_NUMBER>");
 console.log("\nExample:\n> npm run ex5");
